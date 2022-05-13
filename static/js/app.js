77cd7ba6d849cf4d7439a37f03c673046afea037
youroriginal <chienjanzen@gmail.com>
2022-05-13 19:51:14 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -12,8 +12,8 @@ function buildTable(data) {
     // loop through each object and append 
     // row + cells for each value in the row
     data.forEach((dataRow) => {
-        
-        // append row to table body in html
+
+        // append empty row to table body in html 
         let row = tbody.append("tr");
 
         // Loop through each field in the dataRow and add
@@ -27,4 +27,29 @@ function buildTable(data) {
 
     });
 
-}
+};
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
